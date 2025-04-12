1eb2ba5d8429e30a026600e12b9238a18f319bd0
jethibhavya <jethibhavya@gmail.com>
2025-04-12 12:23:12 +0530

    added login page functionality and password reset link

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # This looks for blog/post_list.html meaning <app>/<model>_<view_type>.html
+    path('', PostListView.as_view(), name='blog-home'),
+    path('_post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('_post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('_post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
